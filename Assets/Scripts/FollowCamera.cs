a0ebd3da56cc2867b3c608efa1ef2492010b50c3
Joao Melo <jvictorcostamelo@gmail.com>
2018-11-02 22:01:44 -0300

    centralization

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+    public Transform player;
+    public float posX, posY;
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        posX = transform.position.x;
+        posY = transform.position.y;
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+    }
+
+   /* void OnCollisionEnter2D(Collision2D col)
+    {
+        if (!col.gameObject.CompareTag("Player"))
+            Destroy(col.gameObject);
+
+    }*/
+}
